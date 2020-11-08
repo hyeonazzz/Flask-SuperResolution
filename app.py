2b477f5a0a777a5f91e150211e5bfe881ec6d209
hyeonazzz <nkh@nam.so>
2020-11-08 17:53:03 +0900

    def upload_file

--- app.py
+++ app.py
@@ -6,15 +6,16 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/result')
+def result():
+    return render_template('result.html')
+
+@app.route('/fileUpload', methos = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save('Uploads/'+ secure_filename(f.filename))
         return 'good'
-    
-@app.route('/result')
-def result():
-    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
