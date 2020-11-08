ce70bbe5dd84062cdd3d006ac8702b513f2296cb
hyeonazzz <nkh@nam.so>
2020-11-08 21:58:30 +0900

    def upload

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 import os, sys
-real_path = os.path.dirname(os.path.realpath(__file__))
-sub_path = "/".join(real_path.split("/")[:-1])
-os.chdir(sub_path)
 from flask import Flask, render_template, redirect, url_for, request, escape, Response, g, make_response
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
 uploads_dir = os.path.join(app.instance_path, 'static/img/user_img')
 os.makedirs(uploads_dir, exist_ok=True)
-
-def root_path():
-	real_path = os.path.dirname(os.path.realpath(__file__))
-	sub_path = "\\".join(real_path.split("\\")[:-1])
-	return os.chdir(sub_path)
 
 @app.route('/')
 def index():
