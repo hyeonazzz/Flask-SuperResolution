ba453241ff0ed001a9575cdb8f4ed719f637b2d4
hyeonazzz <nkh@nam.so>
2020-11-08 17:19:12 +0900

    def upload_file

--- app.py
+++ app.py
@@ -2,22 +2,19 @@ from flask import Flask, render_template, redirect, url_for, request
 from werkzeug.utils import secure_filename
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['GET', 'POST'])
 def index():
     return render_template('index.html')
-    
-    
-@app.route('/result')
-def result():
-    return render_template('result.html')
 
-
-@app.route('/fileUpload', methods = ['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         f = request.files['file']
         f.save('Uploads/'+ secure_filename(f.filename))
         return render_template('result.html')
+    
+@app.route('/result')
+def result():
+    return render_template('result.html')
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
