f15fa9b3b6b03ed5713202d54550fc9397318afe
hyeonazzz <nkh@nam.so>
2020-11-20 18:56:05 +0900

    srgan

--- app.py
+++ app.py
@@ -2,9 +2,8 @@ import os, sys
 import time
 from flask import Flask, render_template, redirect, url_for, request, escape, Response, g, make_response
 from werkzeug.utils import secure_filename
-#sys.path.append('DBPN')
-from main import *
-from mode import *
+#from main import *
+#from mode import *
 
 UPLOAD_DIR = 'static/images/user_img'
 app = Flask(__name__)
