b82317f350378fad4e5ff66c58bcb4b889b0cb72
hyeonazzz <nkh@nam.so>
2020-11-20 19:03:48 +0900

    srgan

--- app.py
+++ app.py
@@ -5,6 +5,36 @@ from werkzeug.utils import secure_filename
 import main
 from mode import test_only
 
+parser.add_argument("--LR_path", type = str, default = 'static/images/user_img')
+parser.add_argument("--GT_path", type = str, default = '')
+parser.add_argument("--res_num", type = int, default = 16)
+parser.add_argument("--num_workers", type = int, default = 0)
+parser.add_argument("--batch_size", type = int, default = 16)
+parser.add_argument("--L2_coeff", type = float, default = 1.0)
+parser.add_argument("--adv_coeff", type = float, default = 1e-3)
+parser.add_argument("--tv_loss_coeff", type = float, default = 0.0)
+parser.add_argument("--pre_train_epoch", type = int, default = 8000)
+parser.add_argument("--fine_train_epoch", type = int, default = 4000)
+parser.add_argument("--scale", type = int, default = 1/4)
+parser.add_argument("--patch_size", type = int, default = 24)
+parser.add_argument("--feat_layer", type = str, default = 'relu5_4')
+parser.add_argument("--vgg_rescale_coeff", type = float, default = 0.006)
+parser.add_argument("--fine_tuning", type = str2bool, default = False)
+parser.add_argument("--in_memory", type = str2bool, default = True)
+parser.add_argument("--generator_path", type = str, default = 'model/SRGAN.pt')
+parser.add_argument("--mode", type = str, default = 'test_only')
+
+args = parser.parse_args()
+
+if args.mode == 'train':
+    train(args)
+    
+elif args.mode == 'test':
+    test(args)
+    
+elif args.mode == 'test_only':
+    test_only(args)
+
 UPLOAD_DIR = 'static/images/user_img'
 app = Flask(__name__)
 app.config['UPLOAD_DIR'] = UPLOAD_DIR
